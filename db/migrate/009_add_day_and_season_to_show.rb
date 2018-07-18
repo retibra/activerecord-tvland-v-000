4e36bf528969ade0f250e51bc65ada85f96ab0ba
Timothy Ege <retibra@gmail.com>
2018-07-18 17:25:40 +0000

    automatically backed up by learn

--- db/migrate/009_add_day_and_season_to_show.rb
+++ db/migrate/009_add_day_and_season_to_show.rb
@@ -1,7 +1,8 @@
-class AddCatchphraseToCharacter < ActiveRecord::Migration[4.2]
+class AddDayAndSeasonToShow < ActiveRecord::Migration[4.2]
 
   def change
-    add_column :characters, :catchphrase, :string
+    add_column :shows, :day, :string
+    add_column :shows, :season, :string
   end
 
 end
